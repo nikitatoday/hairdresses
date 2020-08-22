886974fe40e0697029f4cc752d2b620147cc7646
Nikita <volchokplus@gmail.com>
2020-08-22 22:27:34 +0300

    sec

--- js/main.js
+++ js/main.js
@@ -20,3 +20,12 @@ $(function() {
     autoplayHoverPause:true
   });
 });
+
+function myFunction() {
+  const x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
