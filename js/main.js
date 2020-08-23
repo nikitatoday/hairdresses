06d73c76fea50c5fc0ddc4f36a87f58cd2977a38
Nikita <volchokplus@gmail.com>
2020-08-23 20:08:42 +0300

    full

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$('.gallery').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  gallery:{
+    enabled: true
+  }
+  // other options
+});
+
+$(function() {
+  // Owl Carousel
+  const owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    margin: 10,
+    loop: true,
+    nav: false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+  });
+});
+
+$(document).ready(function(){
+
+  $('.header__navicon').click(function(e){
+  	$(this).next().slideToggle(500);
+  });
+
+});
